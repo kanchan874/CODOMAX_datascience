021185fc9d834bf8511e34d62788c74a37c3da8d
kanchan874 <kanchangaikwad2006@gmail.com>
2026-07-23 22:58:33 +0530

    Day 3 NumPy Basics

---
+++ numpy_basics.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50])
+arr2 = np.array([[1, 2, 3],
+                 [4, 5, 6]])
+
+print(arr1)
+print(arr2)
+print(arr2.shape)
+print(arr2.ndim)
+print(arr2.size)
+print(arr2.dtype)
